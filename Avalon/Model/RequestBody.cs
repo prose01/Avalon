9fed1f6a3b6564c1680fb91dd85d9a832a2a68d9
Peter <peterrose03@gmail.com>
2022-01-25 17:42:16 +0100

    Cleanup

--- Avalon/Model/RequestBody.cs
+++ Avalon/Model/RequestBody.cs
@@ -3,7 +3,6 @@
     public class RequestBody
     {
         public ProfileFilter ProfileFilter { get; set; }
-        public OrderByType OrderByType { get; set; }
         public FeedbackFilter FeedbackFilter { get; set; }
     }
 }
