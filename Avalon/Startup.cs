daec342dfb5b4f0b81f318172b69d9900abde7e3
Peter Rose <peter.rose@lrforlag.dk>
2023-12-14 17:43:19 +0100

    Removed AddJsonFile appsettings.{env.EnvironmentName}.json

--- Avalon/Startup.cs
+++ Avalon/Startup.cs
@@ -19,9 +19,9 @@ namespace Avalon
         public Startup(IHostEnvironment env)
         {
             var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
+                //.SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
         }
